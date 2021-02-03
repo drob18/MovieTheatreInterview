75f2268e95d61d938b4ec5a7cc5780128081bd02
David Robinson <65179817+drob18@users.noreply.github.com>
2021-02-03 10:47:09 -0500

    Update Movie.java

--- Movie.java
+++ Movie.java
@@ -30,48 +30,49 @@ public class Movie {
                         boolean seated = false;
                         boolean capacity = true;
                         int samerow = -1;
-                        for (int k = 0; k < seats.length; k++) {
-                            for (int l = 0; l < seats[k].length; l++) {
-                                if (seats[k][l] == 1 && occ > 0 && seats[k].length - l >= occ && !seated) {
-                                    capacity = false;
-                                    seats[k][l] = 0;
-                                    occ--;
-                                    currRes.add(row[k] + Integer.toString(l));
-                                    samerow = k;
-                                } else if (occ == 0 && !seated && samerow == k) {
-                                    int rem = seats[k].length - l;
-                                    if (rem >= 3) {
-                                        seats[k][l] = -1;
-                                        seats[k][l + 1] = -1;
-                                        seats[k][l + 2] = -1;
-                                    } else if (rem == 2) {
-                                        seats[k][l] = -1;
-                                        seats[k][l + 1] = -1;
-                                    } else if (rem == 1) {
-                                        seats[k][l] = -1;
-                                    }
-                                    seated = true;
-                                } else if (occ <= 0 || occ > 20) {
-                                    System.out.print(resID + " Cannot be seated");
-                                    continue;
-                                    
-                            }
-                        }
-                        System.out.println();
-                        if (capacity) {
-                            System.out.print(resID + " Cannot be seated");
-                        } else {
-                            System.out.print(resID + " ");
-                            boolean found = true;
-                            for (String s : currRes) {
-                                if (found == true) {
-                                    System.out.print(s); // no comma
-                                    found = false;
-                                } else {
-                                    System.out.print(", " + s);
+                        if (occ > 0 && occ < 20) {
+                            for (int k = 0; k < seats.length; k++) {
+                                for (int l = 0; l < seats[k].length; l++) {
+                                    if (seats[k][l] == 1 && occ > 0 && seats[k].length - l >= occ && !seated) {
+                                        capacity = false;
+                                        seats[k][l] = 0;
+                                        occ--;
+                                        currRes.add(row[k] + Integer.toString(l));
+                                        samerow = k;
+                                    } else if (occ == 0 && !seated && samerow == k) {
+                                        int rem = seats[k].length - l;
+                                        if (rem >= 3) {
+                                            seats[k][l] = -1;
+                                            seats[k][l + 1] = -1;
+                                            seats[k][l + 2] = -1;
+                                        } else if (rem == 2) {
+                                            seats[k][l] = -1;
+                                            seats[k][l + 1] = -1;
+                                        } else if (rem == 1) {
+                                            seats[k][l] = -1;
+                                        }
+                                        seated = true;
+                                    }      
                                 }
                             }
-                        }
+                            System.out.println();
+                            if (capacity) {
+                                System.out.print(resID + " Cannot be seated");
+                            } else {
+                                System.out.print(resID + " ");
+                                boolean found = true;
+                                for (String s : currRes) {
+                                    if (found == true) {
+                                        System.out.print(s); // no comma
+                                        found = false;
+                                    } else {
+                                        System.out.print(", " + s);
+                                    }
+                                }
+                            }
+                        } else {
+                            System.out.println();
+                            System.out.print(resID + " Cannot be seated");
                     } else {
                         System.out.println("Invalid Reservation");
                         continue;
